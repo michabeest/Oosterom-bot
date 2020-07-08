f0bebd6f6b04d4920ef1ed4e16f64d40b6793eb7
michabeest <67840747+michabeest@users.noreply.github.com>
2020-07-08 19:14:13 +0200

    Update Heer Oosterom.py
    -removed 'kapitalisme' command
    -added 'kcv' command

--- Heer Oosterom.py
+++ Heer Oosterom.py
@@ -1,4 +1,5 @@
 import discord
+import time
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
@@ -9,19 +10,22 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    print(f'{member} has joined a server!')
+    print(f'{member} has joined the server!')
 
 @client.event
 async def on_member_remove(member):
-    print(f'{member} has left a server')
+    print(f'{member} has left the server')
 
 @client.command()
 async def hallo(ctx):
     await ctx.send('hallo! het is weer een mooie dag voor het proletariaat!')
 
-@client.command()
-async def kapitalisme(ctx):
-    await ctx.send('het kapitalisme moet vernietigd worden!')
+@client.command(name='kcv', description = 'ik zal je vertellen wat kcv is')
+async def kcv(ctx):
+    await ctx.send('weet jij niet wat KCV is?!')
+    time.sleep(2)
+    await ctx.send('https://nl.wikipedia.org/wiki/Klassieke_culturele_vorming')
+
 
 
 client.run('NzI5MDMyNDI4NTI5NjQ3NjQ2.XwDCxA.TYZzVhsuTtr2uLhHmM1AFcKssxU')
