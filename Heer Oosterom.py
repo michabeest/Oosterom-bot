2ac014642255ce74117f3723f975e7a9bc7dc49e
michabeest <67840747+michabeest@users.noreply.github.com>
2020-07-09 08:56:16 +0200

    Update Heer Oosterom.py
    added '.ping' command
    added '.help' command
    added functions for bot status

--- Heer Oosterom.py
+++ Heer Oosterom.py
@@ -4,8 +4,11 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
 
+client.remove_command('help')
+
 @client.event
 async def on_ready():
+    await client.change_presence(status = discord.Status.online, activity = discord.Game('retorica'))
     print('bot is ready!')
 
 @client.event
@@ -20,12 +23,27 @@ async def on_member_remove(member):
 async def hallo(ctx):
     await ctx.send('hallo! het is weer een mooie dag voor het proletariaat!')
 
-@client.command(name='kcv', description = 'ik zal je vertellen wat kcv is')
+@client.command(description = 'kcv')
 async def kcv(ctx):
     await ctx.send('weet jij niet wat KCV is?!')
     time.sleep(2)
     await ctx.send('https://nl.wikipedia.org/wiki/Klassieke_culturele_vorming')
 
+@client.command()
+async def waarom(ctx):
+    await ctx.send('daarom')
 
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(title = 'dit kan je met mij doen:', color = discord.Color.red())
+    embed.add_field(name = '.help', value = 'zie de commands die ik heb', inline = False)
+    embed.add_field(name = '.hallo', value = 'zeg hallo', inline = False)
+    embed.add_field(name = '.kcv', value = 'laat mij vertellen over kcv', inline = False)
+    embed.add_field(name = '.ping', value = 'zie mijn ping', inline = False)
+    await ctx.send(embed=embed)
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(round(client.latency*1000))
 
 client.run('NzI5MDMyNDI4NTI5NjQ3NjQ2.XwDCxA.TYZzVhsuTtr2uLhHmM1AFcKssxU')
